12fcccbb179d8ab5ff12c37377c4dbd012dad327
Lucia Perez Alberca <luciaperezalberca@gmail.com>
2024-04-29 14:41:14 -0300

    inicio del proyecto. agrego header y seccion principal de home

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+			  'primary-blue': '#336091',
+			  'primary-orange': '#FF9444',
+			  'secondary-blue': '#8BB6E4',
+			},
+		}
+	},
+	plugins: [],
+}
